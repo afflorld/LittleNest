8f347358d0df3a9ae1c15b0e51c77f0bf677651b
afflorld <sl.adamec@gmail.com>
2024-12-31 12:34:28 +0100

    Fix

--- js/email.js
+++ js/email.js
@@ -6,13 +6,15 @@ function sendMail(){
         email:document.getElementById('email').value,
         subject:"Rezervacia",
         message:document.getElementById('message').value,
-
-
-        
-    
+        date : new Date().toLocaleString(),
+        reply_to: document.getElementById('email').value,
     };
 
+    if(parms.name=="" || parms.email=="" || parms.message==""){
+        alert("Vyplnte vsetky udaje!");
+        return;
+    }else{
     emailjs.send("service_aqrklk3","template_8jp5xrn",parms).then(alert("Dakujeme za vasu rezervaciu!"));
-
+    }
 
 }
